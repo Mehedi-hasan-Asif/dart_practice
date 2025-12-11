b3234d339b405bbdeb3d335ae8891d63b4d51a9a
Mehedi Hasan <mehedihasanprog@gmail.com>
2025-12-11 14:12:14 +0600

    add

---
+++ bin/arithmatic.dart
@@ -0,0 +1,23 @@
+void main(){
+  int num1 = 10;
+  int num2 = 20;
+
+  int sum = num1 + num2;
+  int diff = num1 - num2;
+  int unaryMinus = -num1;
+  double division = num1/num2;
+  int mul= num1 * num2;
+  int mod = num1%num2;
+  int div2 = num1~/num2;
+
+  print ("The addition is $sum.");
+  print ("The addition is $diff.");
+  print ("The addition is $unaryMinus.");
+  print ("The addition is $div2.");
+  print ("The addition is $division.");
+  print ("The addition is $mul.");
+  print ("The addition is $mod.");
+
+
+
+}
